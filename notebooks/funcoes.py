00857e922123827d504cedbe44aee327af8c4ccb
Pedro Corrêa <pedrodealmeid@gmail.com>
2021-06-06 18:31:44 -0300

    update

--- notebooks/funcoes.py
+++ notebooks/funcoes.py
@@ -28,12 +28,12 @@ def million_formatter(x, pos):
     return "%.2f Mi" % (x/1E6)
 
 
-def plota_rel(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=None, xlabel=None, ylabel=None, dict_hue_palette=None,formatter_x=None,
-             formatter_y=None, file_name=None, xlim=None, ylim=None, hue_legend=None, style='darkgrid', show=True, ax=None, 
-             color_title='black', color_sub='dimgray', color_ticks='dimgray', legend=False, legend_title='', title_loc='left', kind='line', **kwargs):
+def plota_grafico(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=None, xlabel=None, ylabel=None, dict_hue_palette=None, palette_sns=None,formatter_x=None,
+                  formatter_y=None,file_name=None, xlim=None, ylim=None, hue_legend=None,style='darkgrid', show=True, ax=None, color_xlabel='dimgray', color_ylabel='dimgray',
+                 color_title='black', color_sub='dimgray', color_xticks='dimgray', color_yticks='dimgray', legend=False, legend_title='', title_loc='left', kind='line', por_ano=False, **kwargs):
     '''
     
-    Função que plota um gráfico de relacionamento entre duas variáveis, podendo ser um 'lineplot' do seaborn ou um 'scatterplot' do seaborn
+    Função que plota um gráfico entre duas variáveis, podendo ser um 'lineplot' do seaborn, 'scatterplot' ou um 'barplot' do seaborn
     
     Parâmentros:
     
@@ -42,6 +42,7 @@ def plota_rel(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=N
             y : coluna associada ao eixo y, tipo : str
             hue : variável associada à tonalidade, tipo : str, padrão : None
             dict_hue_palette : dicionário com o nome da variável e a cor a ser plotada, tipo : dict, padrão : None
+            palette_sns : palheta do seaborn com as cores a serem plotadas, tipo : sns.color_palette, padrão : None
             hue_legend : legenda do grupo que distingue a tonalidade no parâmetro hue, tipo: list, padrão : None
             legend : operador lógico para mostrar a legenda ou não, tipo : boolean(True or False), padrão : False
             legend_title : título da legenda, obs: apenas se legend = True, tipo : str, padrão : ''
@@ -50,55 +51,65 @@ def plota_rel(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=N
             color_title : cor do título, tipo : str, padrão : 'black'
             subtitle : subtítulo do gráfico, tipo : str, padrão : None
             color_sub  : cor do subtítulo, tipo : str, padrão : 'dimgray'
-            color_ticks : cor dos eixos, tipo : str, padrão : 'dimgray'
+            color_xticks : cor do eixo x, tipo : str, padrão : 'dimgray'
+            color_yticks : cor do eixo y, tipo : str, padrão : 'dimgray'
             xlabel : legenda do eixo x, tipo : str, padrão : None 
+            color_xlabel : cor da legenda do eixo x, tipo : str, padrão : 'dimgray'
             xlim : lista com dois valores numéricos indicando o limite do eixo x, tipo : list, padrão : None
             formatter_x : formatador dos números do eixo x, tipo : matplotlib.ticker, padrão : None
             ylabel : legenda do eixo y, tipo : str, padrão : None
+            color_ylabel : cor da legenda do eixo y, tipo : str, padrão : 'dimgray'
             ylim : lista com dois valores numéricos indicando o limite do eixo y, tipo : list, padrão : None
             formatter_y : formatador dos números do eixo y, tipo : matplotlib.ticker, padrão : None
             file_name : nome do arquivo correspondente ao gráfico se quiser ser salvo, obs:por padrão não salva, tipo : str, padrão : None  
             style : stilo do grid do gráfico do seaborn, tipo:str, padrão : 'darkgrid' 
             show : operador lógico para mostrar o gráfico ou não, tipo : boolean(True or False), padrão : True
             ax : eixo se já existir, tipo : matplotlib.pyplot, padrão : Mone
-            kind : tipo do gráfico se é um gráfico de linha ou um scatterplot, tipo : str('line' ou 'scatter'), padrão : 'line'
+            kind : tipo do gráfico se é um gráfico de linha ou um scatterplot, tipo : str('line' ,'scatter' ou 'bar'), padrão : 'line'
+            por_ano : operador lógico se o eixo x será mostrado ano a ano ou não, obs:a coluna relacionada ao ano precisa estar no eixo x e ser do tipo int, tipo : boolean, padrão : False 
             **kwargs : argumentos adicionais para incrementar a chamada do gráfico 'lineplot' ou 'scatterplot' do seaborn
     '''
     
-    #Mudando o estilo de fundo
-    sns.set_style(style)
-    
     #Verificando se foi passado algum eixo 
     if ax == None:
+        #Mudando o estilo de fundo
+        sns.set_style(style)
         #Criando figura e eixos
         fig, ax = plt.subplots(figsize=(16,8)) 
     
+    if palette_sns != None:
+        palette = palette_sns
+    elif dict_hue_palette != None:
+        palette = dict_hue_palette
+    
     #Verificando o tipo do gráfico e plotando
     if kind=='line':
-        sns.lineplot(data=dados, x=x, y=y, hue=hue, palette=dict_hue_palette, ax=ax, **kwargs)
+        sns.lineplot(data=dados, x=x, y=y, hue=hue, palette=palette, ax=ax, **kwargs)
     elif kind=='scatter':
-        sns.scatterplot(data=dados, x=x, y=y, hue=hue, palette=dict_hue_palette, ax=ax, **kwargs)
+        sns.scatterplot(data=dados, x=x, y=y, hue=hue, palette=palette, ax=ax, **kwargs)
+    elif kind == 'bar':
+        sns.barplot(data=dados, x=x, y=y, hue=hue, palette=palette, ax=ax, **kwargs)
 
     #Plotando os títulos, mudando o tamanho e cores das fontes
     plt.sca(ax)
     plt.title(title + '\n', fontsize=25, loc=title_loc, color=color_title)
     plt.text(0,1.03, subtitle, color=color_sub, transform=ax.transAxes, fontsize=15)
-    plt.xlabel(xlabel, color=color_ticks, fontsize=15)
-    plt.ylabel(ylabel, color=color_ticks, fontsize=15)
-    plt.yticks(fontsize=15, color=color_ticks)
+    plt.xlabel(xlabel, color=color_xlabel, fontsize=15)
+    plt.ylabel(ylabel, color=color_ylabel, fontsize=15)
+    plt.yticks(fontsize=15, color=color_yticks)
 
         
     #Verificando se o eixo x se refere ao ano para mostrar todos os anos 
-    if dados[x].dtypes == 'int64':
-        plt.xticks(range(int(min(dados[x])),int(max(dados[x]+1))), range(int(min(dados[x])),int(max(dados[x]+1))), fontsize=15, color=color_ticks)
+    if por_ano == True and dados[x].dtypes == 'int64':
+        plt.xticks(range(int(min(dados[x])),int(max(dados[x]+1))), range(int(min(dados[x])),int(max(dados[x]+1))), fontsize=15, color=color_xticks)
     else:
-        plt.xticks(fontsize=12, color=color_ticks)
+        plt.xticks(fontsize=12, color=color_xticks)
     
     #Verificando se a legenda foi passada como TRUE
     if legend == True:
         handles, labels = ax.get_legend_handles_labels()
         ax.legend(handles=handles[1:], labels=hue_legend, title=legend_title, bbox_to_anchor=(1, 1), fontsize=12, title_fontsize=15)
-    else:
+    elif hue != None:
         plt.legend('')
     
     #Verificando os formatadores dos eixos
@@ -138,7 +149,7 @@ def plota_rel(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=N
     if show:
         #Mostrando o gráfico
         plt.show()
-
+        
 def limpa_casos_regiao(dados_hep_A, dados_hep_B, dados_hep_C, regiao):
     
     hep_A = limpa_casos_h(dados_hep_A, 'A')
