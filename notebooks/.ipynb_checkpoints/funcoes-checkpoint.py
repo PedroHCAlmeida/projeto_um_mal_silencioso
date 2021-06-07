be8d893b6bf231d952568c3924d04734b2416a12
Pedro Corrêa <pedrodealmeid@gmail.com>
2021-06-07 17:28:30 -0300

    update

--- notebooks/.ipynb_checkpoints/funcoes-checkpoint.py
+++ notebooks/.ipynb_checkpoints/funcoes-checkpoint.py
@@ -2,31 +2,124 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import ticker
+import re
 
-def limpa_casos_h(dados, hep):
+def limpa_casos_h(dados:pd.DataFrame, hep:str):
+    
+    '''
+    Função que recebe um dataset com dados sobre casos relacionado à um tipo de hepatite e realiza algumas transformações:
+        
+        Retira as colunas que agrupam diversos anos, coluna 'Total' e coluna '1999-2006'
+        Retira a segunda linha do dataframe que se refere à taxa de incidência e, primeiramente serão usados apenas os dados de casos totais para juntar os dados recentes e mais antigos
+        Transforma a disposição da tabela, transformando as colunas em apenas uma coluna('Ano') atribuindo a classe viral da hepatite(A,B,C) como nome da coluna de valores numéricos
+        Retira a primeira linha que antes se referia ao nome da variável numérica('Casos')
+        Transforma o tipo de dados da coluna 'Ano' em inteiro e a coluna com os dados numéricos para float(podem haver valores nulos) para depois facilitar a manipulação e evitar problemas  
+        
+    Parâmetros:
+        
+        dados : DataFrame onde estão os dados mais recentes dos casos de hepatite, tipo : pd.DataFrame
+        hep : a classe viral da hepatite que o DataFrame se refere, tipo : str
+    
+    Retorno:
+        
+        dados : DataFrame com todas as limpezas realizadas, tipo : pd.DataFrame
+    '''
     
     dados = dados.drop(columns=['Total', '1999-2006'])
     dados = dados.drop(index=1)
     dados = pd.melt(dados, var_name='Ano', value_name=hep)
     dados = dados.drop(0)
-    dados[hep] = dados[hep].astype('int64')
+    dados[hep] = dados[hep].astype('float64')
     dados['Ano'] = dados['Ano'].astype('int64') 
     return dados
 
-@ticker.FuncFormatter
-def thousand_formatter(x, pos):
+        
+def limpa_casos_regiao(dados_hep_A:pd.DataFrame, dados_hep_B:pd.DataFrame, dados_hep_C:pd.DataFrame, regiao:str):
+    
     '''
-    Formata os dados para cada Mil e mostra duas casas decimais depois da vírgula
+    Função que recebe os DataFrames relacionados à cada classe_viral de uma região especifíca e realiza algumas transformações:
+        
+        Chama a função 'limpa_casos_h' para cada tipo de classe viral para realizar uma limpeza em cada DataFrame 
+        Junta os dados de todas as classes virais(A, B, C)
+        Transforma as colunas relacionadas à classe viral em apenas uma coluna('Classe_viral') e atribui a 'regiao' como nome da coluna com os valore numéricos
+    
+    Parâmetros:
+    
+        dados_hep_A : DataFrame onde estão os dados mais recentes dos casos de hepatite A, tipo : pd.DataFrame
+        dados_hep_B : DataFrame onde estão os dados mais recentes dos casos de hepatite B, tipo : pd.DataFrame
+        dados_hep_C : DataFrame onde estão os dados mais recentes dos casos de hepatite C, tipo : pd.DataFrame
+        regiao : Nome da região que o DataFrame se refere, tipo : pd.DataFrame
+        
+    Retorno :
+        
+        dados : retorna um Dataframe com os dados sobre casos de hepatite A, B e C de uma região específica, tipo : pd.DataFrame
     '''
-    return "%.2f Mil" % (x/1E3)
+    
+    hep_A = limpa_casos_h(dados_hep_A, 'A')
+    hep_B = limpa_casos_h(dados_hep_B, 'B')
+    hep_C = limpa_casos_h(dados_hep_C, 'C')
+    dados = pd.merge(hep_A, pd.merge(hep_B, hep_C))
+    dados = pd.melt(dados,'Ano', var_name='Classe_viral', value_name=regiao)
+    
+    return dados
 
-@ticker.FuncFormatter
-def million_formatter(x, pos):
+def limpa_obitos(dados, nome_valores):
+    
     '''
-    Formata os dados para cada Milhão e mostra duas casas decimais depois da vírgula
+    Função que recebe o DataFrame relacionado aos óbitos de cada classe_viral e realiza algumas transformações:
+        
+        Retira as colunas que agrupam diversos anos, coluna 'Total' e coluna '2000-2006' 
+        Retira a palavra 'Hepatite' antes da classe viral(A,B,C)
+        Renomeia a coluna Óbitos por Classe_viral
+        Retira os dados relacionados à Hepatite D
+        Transforma as colunas relacionadas aos Anos em apenas uma coluna('Ano') e atribui o 'nome_valores' como nome da coluna com os valores numéricos
+        Trasnforma o tipo de dados da coluna 'Ano' em inteiro e a coluna com os dados numéricos para float(podem haver valores nulos) a fim de evitar problemas e facilitar a manipulação
+    
+    Parâmetros:
+    
+        dados : DataFrame onde estão os dados mais recentes dos óbitos de hepatite, tipo : pd.DataFrame
+        hep : o nome que a coluna com os valores numéricos vai se referir, podendo ser a região do DataFrame ou a palavra 'Obitos' por exemplo, tipo : str
+        
+    Retorno :
+        
+        dados : retorna um Dataframe com os dados sobre os óbitos de hepatite A, B e C, tipo : pd.DataFrame
     '''
-    return "%.2f Mi" % (x/1E6)
+    
+    dados = dados.drop(columns=['Total','2000-2006'])
+    dados['Óbitos'] = dados.replace(r'Hepatite\s', '', regex=True)
+    dados = dados.rename(columns={'Óbitos':'Classe_viral'})
+    dados = dados.drop(index=3)
+    dados = pd.melt(dados, 'Classe_viral', var_name='Ano', value_name=nome_valores)
+    dados['Ano'] = dados['Ano'].astype('int64')
+    dados[nome_valores] = dados[nome_valores].astype('float64')
+    
+    return dados
 
+def limpa_vacinas_dados(dados:pd.DataFrame, nome_valores:str):
+    
+    '''
+    Função que recebe o DataFrame relacionado às vacinas aplicadas de hepatite podendo ser sobre cobertura de vacinação ou doses aplicadas realiza algumas transformações:
+        
+        Retira as linhas que se referem aos dados totais e aos dados mais recentes de 2021 pois o ano ainda não acabou
+        Retira a palavra 'Hepatite' antes do nome das colunas
+        Renomeia a coluna Óbitos por Classe_viral
+        Transforma as colunas relacionadas à Classe_viral em apenas uma coluna('Classe_viral') e atribui o 'nome_valores' como nome da coluna com os valores numéricos
+    
+    Parâmetros:
+    
+        dados : DataFrame onde estão os dados sobre vacinação da hepatite, tipo : pd.DataFrame
+        nome_valores : o nome que a coluna com os valores numéricos vai se referir, podendo ser 'Cobertura' ou 'Doses_aplicadas' por exemplo, tipo : str
+        
+    Retorno :
+        
+        dados : retorna um Dataframe com os dados sobre vacinação da hepatite, tipo : pd.DataFrame
+    '''
+    
+    dados = dados[(dados['Ano'] != 'Total') & (dados['Ano'] != '2021')]
+    dados = dados.rename(columns=lambda x:re.sub(r'.*Hepatite A.*','A',x))
+    dados = dados.rename(columns=lambda x:re.sub(r'.*Hepatite B.*','B',x))
+    dados = pd.melt(dados, 'Ano', var_name='Classe_viral', value_name=nome_valores)
+    return dados
 
 def plota_grafico(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtitle=None, xlabel=None, ylabel=None, dict_hue_palette=None, palette_sns=None,formatter_x=None,
                   formatter_y=None,file_name=None, xlim=None, ylim=None, hue_legend=None,style='darkgrid', show=True, ax=None, color_xlabel='dimgray', color_ylabel='dimgray',
@@ -35,7 +128,7 @@ def plota_grafico(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtit
     
     Função que plota um gráfico entre duas variáveis, podendo ser um 'lineplot' do seaborn, 'scatterplot' ou um 'barplot' do seaborn
     
-    Parâmentros:
+    Parâmetros:
     
             dados : DataFrame do pandas onde estão os dados, tipo=pd.DataFrame 
             x : coluna associada ao eixo x, tipo : str
@@ -79,8 +172,12 @@ def plota_grafico(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtit
     
     if palette_sns != None:
         palette = palette_sns
+        
     elif dict_hue_palette != None:
         palette = dict_hue_palette
+    
+    else:
+        palette=None
     
     #Verificando o tipo do gráfico e plotando
     if kind=='line':
@@ -146,26 +243,20 @@ def plota_grafico(dados: pd.DataFrame, x: str, y:str, hue=None, title='', subtit
         plt.savefig(f'../images/{file_name}')
 
     #Verificando se foi passado para mostrar o gráfico
-    if show:
+    if show == True:
         #Mostrando o gráfico
         plt.show()
-        
-def limpa_casos_regiao(dados_hep_A, dados_hep_B, dados_hep_C, regiao):
-    
-    hep_A = limpa_casos_h(dados_hep_A, 'A')
-    hep_B = limpa_casos_h(dados_hep_B, 'B')
-    hep_C = limpa_casos_h(dados_hep_C, 'C')
-    dados = pd.merge(hep_A, pd.merge(hep_B, hep_C))
-    dados = pd.melt(dados,'Ano', var_name='Classe_viral', value_name=regiao)
-    
-    return dados
 
-def limpa_obitos_reg(dados, regiao):
-    
-    dados = dados.drop(columns=['Total','2000-2006'])
-    dados['Óbitos'] = dados.replace(r'Hepatite\s', '', regex=True)
-    dados = dados.rename(columns={'Óbitos':'Classe_viral'})
-    dados = dados.drop(index=3)
-    dados = pd.melt(dados, 'Classe_viral', var_name='Ano', value_name=regiao)
-    
-    return dados
+@ticker.FuncFormatter
+def thousand_formatter(x, pos):
+    '''
+    Função responsável por formatar um eixo do 'matplotlib' dividindo os valores por Mil, mostrando duas casas decimais depois da vírgula e colocando a palavra 'Mil' após os valores indicando a grandeza
+    '''
+    return "%.2f Mil" % (x/1E3)
+
+@ticker.FuncFormatter
+def million_formatter(x, pos):
+    '''
+    Função responsável por formatar um eixo do 'matplotlib' dividindo os valores por Milhão, mostrando duas casas decimais depois da vírgula e colocando a palavra 'Mi' após os valores indicando a grandeza
+    '''
+    return "%.2f Mi" % (x/1E6)
